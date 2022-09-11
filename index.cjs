f9ec378c8acea35aa40ca081839e1a489a4a0e35
andystevenson <andystevenson@mac.com>
2022-09-11 13:12:08 +0100

    made hasSameValues verbose

--- index.cjs
+++ index.cjs
@@ -1,6 +1,6 @@
 const util = require('node:util')
 ;(async () => {
   const hasSameValues = require('@andystevenson/lib/hasSameValues')
-  console.log(hasSameValues({ a: 1, b: 2 }, { a: 1, b: 2 }))
-  console.log(hasSameValues({ a: 1, b: 2 }, { a: 1, b: '3' }))
+  console.log(hasSameValues({ a: 1, b: 2 }, { a: 1, b: 2 }, true))
+  console.log(hasSameValues({ a: 1, b: 2 }, { a: 1, b: '3' }, true))
 })()
--- src/hasSameValues.cjs
+++ src/hasSameValues.cjs
@@ -1,9 +1,23 @@
 const { eq } = require('lodash')
+const { inspect } = require('node:util')
+const options = { depth: null, colors: true }
 
-function hasSameValues(a, b) {
+function hasSameValues(a, b, verbose = false) {
   for (const key in a) {
     const equals = eq(a[key], b[key])
-    if (!equals) return false
+    if (!equals) {
+      if (verbose) {
+        const valueA = a[key]
+        const typeA = typeof valueA
+        const displayA = inspect(valueA, options)
+        const valueB = b[key]
+        const typeB = typeof valueB
+        const displayB = inspect(valueB, options)
+        const error = `key ${key} not equal [${typeA},${displayA}] != [${typeB},${displayB}]`
+        console.error(error)
+      }
+      return false
+    }
   }
   return true
 }
